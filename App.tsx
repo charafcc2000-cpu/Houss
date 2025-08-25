8a70c857f1dee0d624578d08c35356f5c15c74de
charafcc2000-cpu <charafcc2000@gmail.com>
2025-08-25 09:26:03 +0100

    Update and rename README.md to App.tsx

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import Index from "./pages/Index";
+import Settings from "./pages/Settings";
+import Adhkar from "./pages/Adhkar";
+import QuranWeb from "./pages/QuranWeb";
+import CompassPage from "./pages/CompassPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <LanguageProvider>
+      <TooltipProvider>
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/adhkar" element={<Adhkar />} />
+            <Route path="/quran-web" element={<QuranWeb />} />
+            <Route path="/compass" element={<CompassPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </LanguageProvider>
+  </QueryClientProvider>
+);
+
+export default App;
